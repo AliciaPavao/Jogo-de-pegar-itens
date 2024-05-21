b840d58c24af50b319f26094fbe0b6afe020dccf
Alicia <aliciapavao369@gmail.com>
2024-05-21 11:25:58 -0300

    minha musica deu certo minhas vidas tbm, to tentando por pra reiniciar

--- jogo.py
+++ jogo.py
@@ -3,15 +3,16 @@ from personagens import Personagem
 from obstaculo import Obstaculo
 
 pygame.init()
+pygame.mixer.init()
 #Construindo a tela
 tela = pygame.display.set_mode((1000, 700))
 pygame.display.set_caption("Jogo do chinbinho")
 tela.fill((117, 75, 19))
 
-musica = pygame.mixer.music.load("Borboletinha.mp3")
+musica = pygame.mixer.Sound("Borboletinha.mp3")
 
 pontuacao = 0
-vida = 5
+vidas = 5
 
 # Carregando imagens
 fundo = Personagem("imagem/fundo.png", 1000, 700, 0, 0 )
@@ -45,10 +46,13 @@ while rodando:
         if evento.type == pygame.QUIT:
             rodando = False        
         if evento.type == pygame.MOUSEBUTTONDOWN:
-            #if vidas >= 0:
+            if vidas >= 0:
+                pygame.MOUSEBUTTONDOWN:
+                    vidas = 5
+                    pontuacao = 0
             pass
 
-    musica = pygame.mixer.music.play()
+    musica.play()
 
     fundo.desenhar(tela)
 
@@ -59,6 +63,7 @@ while rodando:
         if borboleta.mascara.overlap(obstaculo.mascara,(obstaculo.posiçãoX-borboleta.posiçãoX, obstaculo.posiçãoY-borboleta.posiçãoY,)):
             obstaculo.posiçãoY = 1050
             pontuacao = pontuacao - 1
+            vidas = vidas - 1
 
     for obstaculo in lista_itens:
         obstaculo.movimenta()
@@ -74,8 +79,11 @@ while rodando:
     fonte = pygame.font.SysFont("Cooper Black",28, False, False)
 
     texto_pontuacao = fonte.render(f"Pontuação:{pontuacao} ", True, (0, 0, 0))
+    texto_vidas = fonte.render(f"Vidas:{vidas} ", True, (240, 22, 120))
 
-    tela.blit(texto_pontuacao,(30,650))
+    tela.blit(texto_pontuacao,(30,630))
+    tela.blit(texto_vidas,(30,660))
+
 
     # Atualizando a tela
     pygame.display.update()
